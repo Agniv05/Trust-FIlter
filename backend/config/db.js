81984f79e20630ad710a60d78ed4fc48624fb289
Agnivom <agnivmitra11@gmail.com>
2025-07-06 18:15:27 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,16 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', // Change if needed
+  database: 'phone_numbers',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+module.exports = connection;
 
